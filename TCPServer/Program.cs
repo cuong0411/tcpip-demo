5939c12385599dd14efc0b15ea87abe96d3e53a4
Cuong Nguyen <84526626+cuong0411@users.noreply.github.com>
2023-12-19 22:08:04 +0700

    Add project files.

---
+++ TCPServer/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TCPServer
+{
+    public class Program
+    {
+        private const int BUFFER_SIZE = 4096;
+        private const int PORT_NUMBER = 12345;
+        private const string IP_ADDRESS = "127.0.0.1";
+
+        static void Main()
+        {
+            Console.Title = "Server";
+            TcpListener listener = null;
+            try
+            {
+                IPAddress ipAddress = IPAddress.Parse(IP_ADDRESS);
+                listener = new TcpListener(ipAddress, PORT_NUMBER);
+                listener.Start();
+                Console.WriteLine($"Server is listening on {ipAddress}:{PORT_NUMBER}");
+
+                while (true)
+                {
+                    TcpClient tcpClient = listener.AcceptTcpClient();
+                    Console.WriteLine($"Connection received from {tcpClient.Client.RemoteEndPoint}");
+
+                    NetworkStream stream = tcpClient.GetStream();
+                    byte[] bytes = new byte[BUFFER_SIZE];
+                    int bytesRead;
+
+                    while ((bytesRead = stream.Read(bytes, 0, bytes.Length)) != 0)
+                    {
+
+                        string clientMessage = Encoding.UTF8.GetString(bytes, 0, bytesRead);
+                        Console.WriteLine($"Client: {clientMessage}");
+
+                        byte[] response = Encoding.UTF8.GetBytes("Server recieved client's message successfully");
+                        stream.Write(response, 0, response.Length);
+                    }
+                    tcpClient.Close();
+                }
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine($"SocketException: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error {ex.Message}");
+            }
+            finally
+            {
+                if (listener != null) listener.Stop();
+            }
+
+            Console.WriteLine("Server is closed");
+            Console.Read();
+        }
+    }
+}
---
+++ tcp-ip-demo/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCPClient
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Client";
+            try
+            {
+                Console.Write("Enter hostname: ");
+                string serverIP = Console.ReadLine();
+                Console.Write("Enter port: ");
+                int serverPort = Int32.Parse(Console.ReadLine());
+
+                TcpClient tcpClient = new TcpClient(serverIP, serverPort);
+                Console.WriteLine($"Client connected to server {serverIP}:{serverPort}");
+
+                NetworkStream stream = tcpClient.GetStream();
+
+                Console.WriteLine(">>> Enter your message to client (type 'exit' to close connection): ");
+                while (true)
+                {
+                    Console.Write(">>> Client: ");
+                    string message = Console.ReadLine();
+                    if (message.ToLower() == "exit")
+                    {
+                        break;
+                    }
+
+                    byte[] data = Encoding.UTF8.GetBytes(message);
+                    stream.Write(data, 0, data.Length);
+
+                    data = new byte[4096];
+                    int bytesRead = stream.Read(data, 0, 4096);
+                    string serverResponse = Encoding.UTF8.GetString(data, 0, bytesRead);
+                    Console.WriteLine($">>> Server: {serverResponse}");
+                }
+
+                tcpClient.Close();
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine($"SocketException: {e.Message}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error: {e.Message}");
+            }
+
+            Console.WriteLine("Client Disconnected");
+            Console.Read();
+        }
+    }
+}
